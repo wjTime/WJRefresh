5b0cdc46279759dd420617d80af24ab2eb4ac57a
wjTime <576527857@qq.com>
2016-04-08 20:57:54 +0800

    change
    change

--- WJRefresh/WJRefreshDemo/ViewController.m
+++ WJRefresh/WJRefreshDemo/ViewController.m
@@ -22,6 +22,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"WJRefresh";
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.view.backgroundColor = [UIColor whiteColor];
     [self createTableView];
